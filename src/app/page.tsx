8b8569037ff2ca05f9c28f3ddcbddf97133a01bc
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-02-12 10:01:35 +0100

    Server Components vs Client Components with "use client"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import clsx from "clsx";
+import { Header } from "@/components/Header";
 
 export default function HomePage() {
+  console.log("HOMEPAGE")
   return (
     <div>
-      <h1
-        className={clsx(
-          "text-xl font-bold text-blue-500 hover:text-blue-50 hover:bg-blue-500 transition duration-1000",
-        )}
-      >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, optio
-        aperiam sequi ipsa asperiores tempore earum velit harum molestias
-        deserunt dignissimos odit! Voluptatem assumenda consequatur corrupti
-        ratione reiciendis voluptates ipsa!
-      </h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import clsx from "clsx";
+
+export function Header() {
+  console.log("HEADER")
+  return (
+    <h1
+      className={clsx(
+        "text-xl font-bold text-blue-500 hover:text-blue-50 hover:bg-blue-500 transition duration-1000",
+      )}
+      onClick={() => alert(123)}
+    >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, optio
+      aperiam sequi ipsa asperiores tempore earum velit harum molestias
+      deserunt dignissimos odit! Voluptatem assumenda consequatur corrupti
+      ratione reiciendis voluptates ipsa!
+    </h1>
+  )
+}
