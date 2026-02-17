3d0ba49da32c3d961c5a9fb8190eadda2ded064f
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-02-17 09:56:54 +0100

    Add global site container and implement light/dark theme using Tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,15 @@ import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
-
   return (
-    <div>
-      <header className="text-6xl font-bold text-center py-8">
-        Header
-      </header>
+    <div className="text-slate-900 bg-slate-100 min-h-screen">
+      <header className="text-6xl font-bold text-center py-8">Header</header>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
 
-      <footer className="text-6xl font-bold text-center py-8">
-        Footer
-      </footer>
+      <footer className="text-6xl font-bold text-center py-8">Footer</footer>
     </div>
   );
 }
