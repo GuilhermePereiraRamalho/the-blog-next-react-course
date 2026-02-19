126bbafee7dac5d81e6f9b0f15e35de4070b0ea6
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-02-19 10:08:51 +0100

    Create PostImageCover component with Next.js Link and Image integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { PostCoverImage } from "@/components/PostCoverImage";
 import { PostHeading } from "@/components/PostHeading";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import clsx from "clsx";
-import Image from "next/image";
-import Link from "next/link";
+
 import { Suspense } from "react";
 
 export default async function HomePage() {
@@ -16,16 +16,18 @@ export default async function HomePage() {
       <section
         className={clsx("grid grid-cols-1 gap-8 mb-16 group", "sm:grid-cols-2")}
       >
-        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
-          <Image
-            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
-            src="/images/bryen_0.png"
-            width={1200}
-            height={720}
-            alt="Post title"
-            priority
-          />
-        </Link>
+        <PostCoverImage
+          linkProps={{
+            href: "/posts/dsada",
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: "/images/bryen_9.png",
+            alt: "image alt",
+            priority: true
+          }}
+        />
         <div className={clsx("flex flex-col gap-4", "sm:justify-center")}>
           <time
             className="text-slate-600 block text-sm/tight"
@@ -33,7 +35,9 @@ export default async function HomePage() {
           >
             18/02/2026 09:30
           </time>
-          <PostHeading as="h1" url="#">Lorem ipsum dolor sit amet.</PostHeading>
+          <PostHeading as="h1" url="#">
+            Lorem ipsum dolor sit amet.
+          </PostHeading>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero at
           quidem hic aut dolorem, tempore autem libero recusandae est minima
           necessitatibus vitae tempora vel cupiditate mollitia aliquam labore.
