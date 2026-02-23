f268172e05293c22c807929d0e671d0ff94a9c18
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-02-23 10:04:25 +0100

    Use React cache to prevent duplicate API or repository calls

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,10 +1,12 @@
 import clsx from "clsx";
 import { PostCoverImage } from "../PostCoverImage";
 import { PostSummary } from "../PostSummary";
+import { findAllPublicPosts } from "@/lib/post/queries";
 
-export function PostFeatured() {
-  const slug = "aaa";
-  const postLink = `/post/${slug}`;
+export async function PostFeatured() {
+  const posts = await findAllPublicPosts();
+  const post = posts[0];
+  const postLink = `/post/${post.slug}`;
   return (
     <section
       className={clsx("grid grid-cols-1 gap-8 mb-16 group", "sm:grid-cols-2")}
@@ -16,19 +18,17 @@ export function PostFeatured() {
         imageProps={{
           width: 1200,
           height: 720,
-          src: "/images/bryen_9.png",
-          alt: "image alt",
+          src: post.coverImageUrl,
+          alt: post.title,
           priority: true,
         }}
       />
       <PostSummary
         postLink={postLink}
         postHeading="h1"
-        createdAt={"2025-04-08T00:24:38.616Z"}
-        title={"Rotina matinal de pessoas altamente eficazes"}
-        excerpt={
-          "O Next.js também é uma boa escolha para quem quer se preocupar com performance e SEO."
-        }
+        createdAt={post.createdAt}
+        title={post.title}
+        excerpt={post.excerpt}
       />
     </section>
   );
